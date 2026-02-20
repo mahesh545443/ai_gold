45cb8399ada9f8a22973330b94844129f29e608f
Mahesh <148073692+mahesh545443@users.noreply.github.com>
2026-02-20 19:31:25 +0530

    Update classifier.py

--- agents/classifier.py
+++ agents/classifier.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from pathlib import Path
+import streamlit as st
 
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
@@ -18,7 +19,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 class ClassificationAgent:
     def __init__(self):
         self.api_url = "https://api.groq.com/openai/v1/chat/completions"
-        self.token = os.getenv("GROQ_API_KEY")
+        self.token = st.secrets["GROQ_API_KEY"]
         if not self.token:
             raise ValueError("❌ GROQ_API_KEY missing from .env!")
         self.model = "llama-3.3-70b-versatile"
